de5a298649d2f569237583a7f3bc567a4c1acbb6
FrancescFe <fraferru@gmail.com>
2026-02-17 16:26:43 +0100

    feat (037): reorganize dependencies and plugins

--- spec/api-catalog/build.gradle.kts
+++ spec/api-catalog/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.1.21"
-    kotlin("plugin.spring") version "2.1.21"
-    id("org.openapi.generator") version "7.15.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.openapi.generator)
     `maven-publish`
-    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.2.0"
+    alias(libs.plugins.artifactregistry)
 }
 
 version = providers.gradleProperty("packageVersion").orElse("0.1.0").get()
--- spec/ms-catalog/build.gradle.kts
+++ spec/ms-catalog/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    kotlin("jvm") version "2.1.21"
-    kotlin("plugin.spring") version "2.1.21"
-    id("org.openapi.generator") version "7.15.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.openapi.generator)
     `maven-publish`
-    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.2.0"
+    alias(libs.plugins.artifactregistry)
 }
 
-version = providers.gradleProperty("packageVersion").orElse("1.5.0").get()
+version = providers.gradleProperty("packageVersion").orElse("1.5.1").get()
 
 apply(from = rootProject.file("gradle/spec-module.gradle"))
